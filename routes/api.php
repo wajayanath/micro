9b55e443cf9a249852ba78759b19f74bf33b36e5
wajayanath <wajayanath@gmail.com>
2021-01-30 12:48:20 +0530

    upload image

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,10 @@ Route::group(['middleware' => 'auth:api'], function (){
     Route::get('user', 'UserController@user');
     Route::put('users/info', 'UserController@updateInfo');
     Route::put('users/password', 'UserController@updatePassword');
+    Route::post('upload', 'ImageController@upload');
 
     Route::apiResource('users', 'UserController');
     Route::apiResource('roles', 'RoleController');
+    Route::apiResource('products', 'ProductController');
 });
 
