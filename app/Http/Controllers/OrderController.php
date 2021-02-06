44eb4eae8963e2f937e3c96367b10627db698127
wajayanath <wajayanath@gmail.com>
2021-02-06 10:09:30 +0530

    export csv

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\OrderResource;
 use App\Order;
+use function fclose;
+use function fopen;
+use function fputcsv;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -18,5 +21,38 @@ class OrderController extends Controller
     public function show($id)
     {
         return new OrderResource(Order::find($id));
+    }
+
+    public function export()
+    {
+        $headers = [
+            "content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=orders.csv",
+            "Pragma" => "no-cache",
+            "Cache-control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0",
+        ];
+
+        $callback = function () {
+            $orders = Order::all();
+            $file = fopen('php://output', 'w');
+
+            //header row
+            fputcsv($file, ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']);
+
+            //body
+            foreach ($orders as $order) {
+                fputcsv($file, [$order->id, $order->name, $order->email, '', '', '']);
+
+                foreach ($order->orderItems as $orderItem) {
+                    fputcsv($file, ['', '', '', $orderItem->product_title, $orderItem->price,  $orderItem->quantity]);
+                }
+            }
+
+            fclose($file);
+
+        };
+
+        return \Response::stream($callback, 200, $headers);
     }
 }
