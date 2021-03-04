baef564239bbaa15ef29d275c6d7b2e388c0973c
wajayanath <wajayanath@gmail.com>
2021-03-04 22:00:18 +0530

    list users

---
+++ vue-admin/src/secure/users/Users.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h2>Section title</h2>
+        <div class="table-responsive">
+            <table class="table table-striped table-sm">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="user in users" :key="user.id">
+                    <td>{{user.id}}</td>
+                    <td>{{user.first_name}} {{user.last_name}}</td>
+                    <td>{{user.email}}</td>
+                    <td>{{user.role.name}}</td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { ref, onMounted } from 'vue';
+    import axios from 'axios';
+
+    export default {
+        name: "Users",
+        setup() {
+            const users = ref([]);
+
+            onMounted(async ()=> {
+                const response = await axios.get('users');
+
+                users.value = response.data.data;
+            });
+
+            return {
+                users
+            }
+        }
+    }
+</script>
+
