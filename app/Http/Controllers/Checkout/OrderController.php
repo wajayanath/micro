a3036be21c41bfd75c757901acb2ebc49ec3d093
wajayanath <wajayanath@gmail.com>
2021-05-06 09:38:19 +0530

    creating orders

---
+++ app/Http/Controllers/Checkout/OrderController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Checkout;
+
+use App\Link;
+use App\Order;
+use App\OrderItem;
+use Illuminate\Http\Request;
+
+class OrderController
+{
+    public function store(Request $request)
+    {
+        $link = Link::whereCode($request->input('code'))->first();
+        $order = new Order();
+
+        $order->first_name = $request->input('first_name');
+        $order->last_name = $request->input('last_name');
+        $order->email = $request->input('email');
+        $order->code = $link->code;
+        $order->user_id = $link->user->id;
+        $order->influencer_email = $link->user->email;
+        $order->address = $request->input('address');
+        $order->address2 = $request->input('address2');
+        $order->city = $request->input('city');
+        $order->country = $request->input('country');
+        $order->zip = $request->input('zip');
+
+        $order->save();
+
+        foreach ($request->input('items') as $item) {
+            $product = Product::find($item['product_id']);
+
+            $orderItem = new OrderItem();
+            $orderItem->order_id = $order->id;
+            $orderItem->product_title = $product->title;
+            $orderItem->price = $product->price;
+            $orderItem->quantity = $item['quantity'];
+            $orderItem->influencer_revenue = 0.1 * $product->price * $item['quantity'];
+            $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
+
+            $orderItem->save();
+        }
+
+        return $order;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -64,4 +64,5 @@ Route::group([
     'namespace' => 'Checkout'
 ], function (){
     Route::get('links/{code}', 'LinkController@show');
+    Route::post('orders', 'OrderController@store');
 });
